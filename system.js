9af1c344a600adfe62bc04fc202bee3f5ff35ce9
Mzili Jalil <jalil.mzili@epitech.eu>
2025-12-10 15:38:51 +0100

    clear code

--- system.js
+++ system.js
@@ -2,10 +2,10 @@
 class CameraA {
 constructor(location) {
     this.location = location;
-} // location = nom de la pièce
+} 
 detect() {
     return `Alerte caméra détectée dans la pièce: ${this.location}`;
-} // Déclenche un message d’alerte (string)
+} 
 }
 
 // TemperatureSensorA
@@ -13,10 +13,10 @@ detect() {
 constructor(location, threshold) {
     this.location = location;
     this.threshold = threshold;
-} // location = nom de la pièce, threshold = température de déclenchement
+} 
  detect() {
     return `Alerte température élevée dans la pièce: ${this.location}, seuil: ${this.threshold}°C`;
- } // Déclenche un message d’alerte (string)
+ } 
  }
 
 // MotionSensorA
@@ -26,7 +26,7 @@ class MotionSensorA {
  }
  detect() {
     return `Alerte mouvement détecté dans la pièce: ${this.location}`;
- } // Déclenche un message d’alerte (string)
+ } 
  }
 
 
@@ -34,14 +34,14 @@ class MotionSensorA {
 class ThermalSensorB {
 constructor(position) {
     this.position = position;
-} // position {
+} // position 
 scanHeatSignature() {
     return {
         "sensor": this.position,
         "detection": "thermal",
         "date": new Date().toISOString()
     };
-} // Déclenche une donnée complexe json, voir ci dessous
+} 
 }
   
 console.log(new ThermalSensorB("Hall d'entrée").scanHeatSignature());
